7b027272d8ca0518b613337c601e8bb678d3b27f
Luis Ramirez <luis21x@outlook.com>
2021-09-14 22:27:19 -0400

    massive code refactor, if else conditionals replaced with confirm prompts and addition assignment to a character available variable

--- script.js
+++ script.js
@@ -4,123 +4,37 @@ var listNumbers = '123456789';
 var listLowercase = 'abcdefghijklmnopqrstuvwxyz';
 var listUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var listSpecialChar ='!#$%&*+';
-var charAvailable;
-
-
 
 
 function generatePassword(){
-  var passwordLength = prompt('Enter password length. Must be between 8-128 characters.');
-  var lowercase = prompt('Include lowercase characters? Enter Y or N.').toLowerCase();
-  var uppercase = prompt('Include uppercase characters? Enter Y or N.').toLowerCase();
-  var numeric = prompt('Include numbers? Enter Y or N.').toLowerCase();
-  var specialChar = prompt('Inlcude special characters? Enter Y or N.').toLowerCase();
   
   var passwordArr = [];
+  var charAvailable = '';
+  var passwordLength = prompt('Enter password length. Must be between 8-128 characters.');
+  var lowercase = confirm('Include lowercase characters?');
+  var uppercase = confirm('Include uppercase characters?');
+  var numeric = confirm('Include numbers?');
+  var specialChar = confirm('Include special characters?');
   
+   
+  //if user confirms, that list of criteria is added to the available options
+  if (lowercase) {charAvailable += listLowercase};
+  if (uppercase) {charAvailable += listUppercase};
+  if (numeric) {charAvailable += listNumbers};
+  if (specialChar) {charAvailable += listSpecialChar};
   
-  
- /* function numGen() {
-   randomNum = Math.floor(Math.random() * passwordLength);
-   return randomNum;
-  }*/
-
   //pulls random characters from single or multiple lists depending on UI
-  function randomChar() {
-    charAvailable.charAt(Math.floor(Math.random() * charAvailable.length));
     for (i = 0; i < passwordLength ; i++) {
       passwordArr[i] = charAvailable.charAt(Math.floor(Math.random() * charAvailable.length));
     }  
-  }
-
-  function lowcaseGen(){
-    var a = listLowercase.charAt(Math.floor(Math.random() * listLowercase.length));
-    return a;
-  }
-
-  function uppercaseGen(){
-    var b = listUppercase.charAt(Math.floor(Math.random() * listUppercase.length));
-    return b;
-  }
-
-  function numericGen(){
-    var c = listNumbers.charAt(Math.floor(Math.random() * listNumbers.length));
-    return c;
-  }
-
-  function specialGen(){
-    var d = listSpecialChar.charAt(Math.floor(Math.random() * listSpecialChar.length));
-    return d;
-  }
-
   
-  
-  if (lowercase === 'y' && uppercase === 'y' && numeric === 'y' && specialChar === 'y') {
-      charAvailable = listLowercase + listUppercase + listNumbers + listSpecialChar;
+  //converts array content into a string
+  passwordArr = passwordArr.join('');
+  return passwordArr;
 
-      randomChar();
-      passwordArr[passwordLength-2] = lowcaseGen();
-      passwordArr[0] = uppercaseGen();
-      passwordArr[2] = numericGen();
-      passwordArr[passwordLength-3] = specialGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-      
-  } else if (lowercase === 'y' && uppercase === 'y' && numeric === 'y' && specialChar === 'n') {
-      charAvailable = listLowercase + listUppercase + listNumbers;
-      randomChar();
-      passwordArr[passwordLength-2] = lowcaseGen();
-      passwordArr[0] = uppercaseGen();
-      passwordArr[2] = numericGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-    
-  } else if (lowercase === 'y' && uppercase === 'y' && numeric === 'n' && specialChar === 'n') {
-      charAvailable = listLowercase + listUppercase;
-      randomChar();
-      passwordArr[passwordLength-2] = lowcaseGen();
-      passwordArr[0] = uppercaseGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-  } else if (lowercase === 'y' && uppercase === 'n' && numeric === 'n' && specialChar === 'n') {
-      charAvailable = listLowercase;
-      randomChar();
-      passwordArr[passwordLength-2] = lowcaseGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-  } else if (lowercase === 'n' && uppercase === 'y' && numeric === 'n' && specialChar === 'n') {
-      charAvailable = listUppercase;
-      randomChar();
-      passwordArr[0] = uppercaseGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-  } else if (lowercase === 'n' && uppercase === 'n' && numeric === 'y' && specialChar === 'n') {
-      charAvailable = listNumbers;
-      randomChar(); 
-      passwordArr[2] = numericGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-  } else if (lowercase === 'n' && uppercase === 'n' && numeric === 'n' && specialChar === 'y') {
-      charAvailable = listSpecialChar;
-      randomChar();
-      passwordArr[passwordLength-3] = specialGen();
-      passwordArr = passwordArr.join('');
-      console.log(passwordArr);
-      return passwordArr;
-  }
+
 };
-
   
-
-  
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
