1e5c1ad8d7f587a99d33cec2f4482d1042723674
Luis Ramirez <luis21x@outlook.com>
2021-09-14 23:02:54 -0400

    adding backstops for requiring use to enter data based on password length and selecting at least one criteria

--- script.js
+++ script.js
@@ -3,20 +3,39 @@ var generateBtn = document.querySelector("#generate");
 var listNumbers = '123456789';
 var listLowercase = 'abcdefghijklmnopqrstuvwxyz';
 var listUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var listSpecialChar ='!#$%&*+';
+var listSpecialChar ='!#$%&*+[]{}_|~`/?><=;:-()';
 
 
 function generatePassword(){
   
   var passwordArr = [];
   var charAvailable = '';
-  var passwordLength = prompt('Enter password length. Must be between 8-128 characters.');
-  var lowercase = confirm('Include lowercase characters?');
-  var uppercase = confirm('Include uppercase characters?');
-  var numeric = confirm('Include numbers?');
-  var specialChar = confirm('Include special characters?');
-  
-   
+  var passwordLength;
+  var lowercase;
+  var uppercase;
+  var numeric;
+  var specialChar;
+
+  //check to see if length entered is within range, if not the prompt is called again without calling generatePassword();
+  function enterNumber(){
+  passwordLength = prompt('Enter password length. Must be 8-128 characters.');
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert('Please enter a number greater than 7 and less than 129');
+      enterNumber();
+    }};
+  enterNumber();
+
+  function enterCriteria(){  
+  lowercase = confirm('Include lowercase characters?');
+  uppercase = confirm('Include uppercase characters?');
+  numeric = confirm('Include numbers?');
+  specialChar = confirm('Include special characters?');
+   if(!lowercase && !uppercase && !numeric && !specialChar){
+     alert('Please confirm at least one of the criteria.');
+     enterCriteria();
+   }
+  }
+  enterCriteria();
   //if user confirms, that list of criteria is added to the available options
   if (lowercase) {charAvailable += listLowercase};
   if (uppercase) {charAvailable += listUppercase};
@@ -31,17 +50,13 @@ function generatePassword(){
   //converts array content into a string
   passwordArr = passwordArr.join('');
   return passwordArr;
-
-
 };
   
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
